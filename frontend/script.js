a9fd3e2b7ddebb465b704989b81bf434259e99db
Andras Varga <zoryandras@gmail.com>
2022-02-16 18:02:37 +0100

    so many stuff

--- frontend/script.js
+++ frontend/script.js
@@ -1,115 +1,23 @@
-// console.log(a)
-// console.log(b)
+// window.addEventListener("load", function(){
+//     console.log("My first function!")
+// })
 
+function loadEvent() {
+    console.log("My second function!");
+    let rootElement = document.getElementById("root")
 
+    let card = function(movieReceived){
+        return `
+        <div class="card">
+            <h2>${movieReceived.title}</h2>
+            <div class="time">${movieReceived.year}</div>
+            <div class="rate">${movieReceived.rate}</div>
+        </div>
+    `}
 
-
-// // console.log("Hey Macarena")
-// // console.log('Hey Macarena')
-// // console.log(`Hey 
-// // Macarena`)
-
-// // console.log(typeof 2)
-// // console.log(typeof NaN)
-// // console.log(0/0)
-// // console.log(2.14)
-
-// // console.log(true)
-// // console.log(false)
-
-// // console.log(undefined)
-// // console.log(null)
-
-// // console.log(typeof undefined)
-// // console.log(typeof null)
-
-// // console.log({
-// //     "title": "Start Coding",
-// //     "type": "Solo",
-// //     "difficulty": 4.1,
-// //     "usefulness": 4.2,
-// //     "enjoyability": 3.7,
-// //     "materials": 3.4,
-// //     "tasks": [
-// //         {
-// //             "title": "Bank accounts",
-// //             "description": "",
-// //         },
-// //         {
-// //             "title": "100 doors",
-// //             "description": ""
-// //         },
-// //         {
-// //             "title": "Word filter",
-// //             "description": ""
-// //         }
-
-// //     ]
-// // })
-
-// var a = "Hello1"
-// let b = "Hello2"
-// const c = "Hello3"
-
-// console.log(a)
-// console.log(b)
-// console.log(c)
-
-// console.log(a)
-// console.log(b)
-// console.log(c)
-
-// function d(){
-//     if (1+1 === 2){
-//         var a = "Hello1"
-//         let b = "Hello2"
-//         const c = "Hello3"
-//     }
-//     console.log(a)
-//     console.log(b)
-//     console.log(c)
-// }
-// d()
-
-// let e = 1
-// console.log(e)
-// e = 2
-// console.log(e)
-// e = e*2
-// console.log(e)
-
-// function multiplyByTwo(num){
-//     console.log(num)
-//     return num*2
-// }
-
-// let f = multiplyByTwo(1)
-// console.log(f)
-// console.log(multiplyByTwo(1));
-// console.log(num);
-
-// let g = {
-//     myMethod: function(){
-//         console.log("Hello!");
-//     }
-// }
-// g.myMethod()
-
-// let window = {
-//     addEventListener: function (eventName, eventFunction){
-//         if (eventName === "load") {
-//             eventFunction()
-//         }
-//     }
-// }
-// // window.addEventListener("load", loadEvent)
-
-let h = {
-    anotherMethod: function(text){
-        return `You sent the following argument to this method: ${text}`
+    for (const movieSend of movies) {
+        rootElement.insertAdjacentHTML("beforeend", card(movieSend));
     }
 }
 
-let i = h.anotherMethod("This Kiki is marvellous!")
-console.log(i);
-console.log(h.anotherMethod("surprise bitch"));
+window.addEventListener("load", loadEvent);
