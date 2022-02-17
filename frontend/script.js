6bba627c2117bd65e976759bcba2b9b8a7c713e0
Andras Varga <zoryandras@gmail.com>
2022-02-17 18:33:47 +0100

    changes to script.js

--- frontend/script.js
+++ frontend/script.js
@@ -14,23 +14,25 @@ function loadEvent() {
             <div class="time">${year}</div>
             <div class="rate">${rate}</div>
         </div>
-                `;
-        }
+        `;
+    }
 
     let renderAllCardElements = function(incomingMoviesArray){
-        let renderedCardList = " ";
+        let renderedCardList = `<div class="cards">`;
         // for ciklus ami végigmegy a cardsArray-en
         // minden lépcsőjénél hozzáadja a renderedCardList-hez az adott elemet a megfelelő div cardban
-        // return az elkészült elemekkelk feltöltött card listet (stringekkel)
+        // return az elkészült elemekkel feltöltött card listet (stringekkel)
         for (const incomingMovie of incomingMoviesArray) {
-            renderedCardList += `
-            <div class="card">
-                <h2>${incomingMovie.title}</h2>
-                <div class="time">${incomingMovie.year}</div>
-                <div class="rate">${incomingMovie.rate}</div>
-            </div>
-                `
+            renderedCardList += card2(incomingMovie.title, incomingMovie.year, incomingMovie.rate)
+            // renderedCardList += `
+            // <div class="card">
+            //     <h2>${incomingMovie.title}</h2>
+            //     <div class="time">${incomingMovie.year}</div>
+            //     <div class="rate">${incomingMovie.rate}</div>
+            // </div>
+            //     `
         }
+        renderedCardList += `</div>`
         console.log(renderedCardList)
         return renderedCardList
     }
@@ -40,10 +42,6 @@ function loadEvent() {
     let newGoodMovies = [];
 
     for (const movieSend of movies) {
-        // let newerThan2000 = false;
-        // let floorRate = Math.floor(movieSend.rate);
-
-
         if (movieSend.year > 2000 && movieSend.rate >= 8) {
             newGoodMovies.push(movieSend);
             // rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, floorRate));
