d8bb03ee0c4706a784388b6fa0648ff6aade147b
Andras Varga <zoryandras@gmail.com>
2022-02-18 11:12:13 +0100

    Messing around!
    Co-authored-by: Kinga Kohari <kingakohari@users.noreply.github.com>
    Co-authored-by: Adorjan Sapi <AdySapi@users.noreply.github.com>
    Co-authored-by: GogoMogo1989 <GogoMogo1989@users.noreply.github.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,45 +1,19 @@
-// window.addEventListener("load", function(){
-//     console.log("My first function!")
-// })
+function loadEvent(){
 
-function loadEvent() {
-    console.log("My second function!");
     let rootElement = document.getElementById("root")
-
-    // let card = function(movieReceived){
-    // return `
-    //     <div class="card">
-    //          <h2>${movieReceived.title}</h2>
-    //          <div class="time">${movieReceived.year}</div>
-    //          <div class="rate">${movieReceived.rate}</div>
-    //      </div>
-    //  `}
 
     let card2 = function(title, year, rate){
         return `
         <div class="card">
             <h2>${title}</h2>
-            <div class="time">${year}</div>
-            <div class="rate">${rate}</div>
+            <h4>${year}</h4>
+            <h5>${rate}</h5>
         </div>
-    `}
-
-    // for (const movieSend of movies) {
-    // rootElement.insertAdjacentHTML("beforeend", card2(movieSend));
-    // }
-
-    let anotherFavouriteMovie = {
-    "title": "The Last Scout Boy",
-    "year": 1991,
-    "rate": 7.0,
-    }
-
-    rootElement.insertAdjacentHTML("beforeend", card2(anotherFavouriteMovie.title, anotherFavouriteMovie.year, anotherFavouriteMovie.rate));
+        `;
+    } 
 
     for (const movieSend of movies) {
-        rootElement.insertAdjacentHTML("beforeend",  card2(anotherFavouriteMovie.title, anotherFavouriteMovie.year, anotherFavouriteMovie.rate));
+        rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, movieSend.rate))
     }
-    console.log(movies)
 }
-
 window.addEventListener("load", loadEvent);
